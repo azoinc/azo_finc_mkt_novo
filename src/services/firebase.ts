44795a6cb11a3c426420c8b99833f2aea5c7cf45
Bruno Mossato <bhmossato@gmail.com>
2026-03-09 14:29:09 -0300

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -14,4 +14,4 @@ export const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getFirestore(app, "fincmktazo");
