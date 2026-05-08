c179bacb4150d207ac78e68e8a89d9cff5592aa7
Bruno Mossato <bhmossato@gmail.com>
2026-05-08 16:52:08 -0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -13,7 +13,6 @@ interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
   readonly VITE_SUPABASE_ANON_KEY: string
   
-  // Sienge
   readonly VITE_SIENGE_SUBDOMAIN?: string
   readonly VITE_SIENGE_API_USER?: string
   readonly VITE_SIENGE_API_PASSWORD?: string
